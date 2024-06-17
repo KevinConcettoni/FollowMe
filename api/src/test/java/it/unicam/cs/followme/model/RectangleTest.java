6adb9f08f3e2b117cd72580061d26cdbef09deec
KevinConcettoni <kevin.concettoni@hotmail.com>
2024-06-17 14:14:24 +0200

    Add project

--- api/src/test/java/it/unicam/cs/followme/model/RectangleTest.java
+++ api/src/test/java/it/unicam/cs/followme/model/RectangleTest.java
@@ -2,8 +2,6 @@ package it.unicam.cs.followme.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
